65557eda732dd52598246d1dfe93b56a1233c0eb
grael@comcast.net <cjgtsm79!>
2016-02-18 16:13:42 -0700

    added urls applicable to the Jenkins Sensei App

--- jenkins_sensei/urls.py
+++ jenkins_sensei/urls.py
@@ -6,5 +6,16 @@ urlpatterns = [
     # url(r'^$', 'jenkins_sensei.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^account/', include('registration.backends.default.urls')),
+    url(r'^$', 'jenkins_sensei_quiz.views.index', name='index'),
+#    url(r'^results/', 'jenkins_sensei_quiz.views.results', name='results'),
+#    url(r'^account_administration/', 'jenkins_sensei_quiz.views.account_administration', name='account_administration'),
+    url(r'^quiz_selection/', 'jenkins_sensei_quiz.views.quiz_selection', name='quiz_selection'),
+#    url(r'^quiz_selection/', include('jenkins_sensei_quiz.urls', namespace="jenkins_sensei_quiz")),
+    url(r'^quiz/', include('jenkins_sensei_quiz.urls', namespace="jenkins_sensei_quiz")),
+    url(r'^quiz/', 'jenkins_sensei_quiz.views.git_quiz', name='git_quiz'),
+    url(r'^resources/', 'jenkins_sensei_quiz.views.resources', name='resources'),
+    url(r'^admin/', include(admin.site.urls)),
+
     url(r'^admin/', include(admin.site.urls)),
 ]
